a6d59cf160a6ae0892f81bc2bd7f8da845110351
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-05-22 12:17:06 -0300

    Primeiro Model-View-Controller-Department

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesWebMvc2.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace SalesWebMvc2.Models
+namespace SalesWebMvc2.Models.ViewModels
 {
     public class ErrorViewModel
     {
