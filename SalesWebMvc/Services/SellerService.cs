249478f6f15f014dbc5dc811980cd423d5bc8fc0
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-05-27 19:09:22 -0300

    Chave estrangeira nao nula

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -18,6 +18,7 @@ namespace SalesWebMvc2.Services
         }
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
