2db847522c48c072cd4f635e34033786928afb9d
Rafael Rodrigues <rrobrigues15@gmail.com>
2026-05-28 19:34:27 -0300

    Deletar vendedor

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,7 +8,6 @@ namespace SalesWebMvc2.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
         public string Email { get; set; }
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
